fa4c6be3fc731b4fdeeee66fd3e646b74022a728
adaunais <adaunais@yahoo.com>
2026-03-18 15:06:51 -0400

    Add files via upload

---
+++ RockPaperScissors.py
@@ -0,0 +1,58 @@
+import random 
+
+while True:
+
+    playerScore = 0
+    computerScore = 0
+    choice = ''
+
+
+
+    while choice != 'quit' and computerScore < 5 and playerScore < 5:
+        options = ["rock", "paper", "scissors", "quit"]
+        computerOptions = ["rock", "paper" , "scissors"]
+ 
+
+        print(f"Players Score: {playerScore}\n")
+        print(f"Opponents Score: {computerScore}\n")
+
+        print("This is a game of rock paper scissors!\n")
+        print("Make your choice player!\n")
+        choice = input("ROCK, PAPER, OR SCISSORS!\n")
+        choice = choice.lower()
+        computerChoice = random.choice(computerOptions)
+
+        if choice not in options:
+            print("Invalid choice, try again.")
+            continue
+
+
+
+        if choice == computerChoice:
+            print("ITS A TIE")
+
+        elif choice == "rock" and computerChoice == "scissors" or choice == "paper" and computerChoice == "rock" or choice == "scissors" and computerChoice == "paper":
+            print(f"You chose {choice}\n")
+            print(f"Oppenent chose {computerChoice}\n")
+            print("YOU WIN!\n")
+            playerScore = playerScore + 1
+
+        elif choice == "rock" and computerChoice == "paper" or choice == "paper" and computerChoice == "scissors" or choice == "scissors" and computerChoice == "rock":
+            print(f"You chose {choice}\n")
+            print(f"Oppenent chose {computerChoice}\n")
+            print("YOU LOSE!\n")
+            computerScore = computerScore + 1
+        
+        elif choice == 'quit' or playerScore == 5 or computerScore ==5:
+            break
+
+    print("thank you for playing!")
+    again = input("Do you want to play again? (yes/no): ").lower()
+    if again != "yes":
+        print("Thanks for playing!")
+        break
+
+
+
+
+
